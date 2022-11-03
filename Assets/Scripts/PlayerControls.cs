fb3c1be108d20b363cb42b2b6e13658da43349a8
Maxim Kravchenko <max.kravchenko.dev@gmail.com>
2022-11-03 09:54:01 +0200

    Start Moving Our Player

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerControls : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        float xThrow = Input.GetAxis("Horizontal");
+        float yThrow = Input.GetAxis("Vertical");
+
+        float xOffset = .1f;
+        float newXPos = transform.localPosition.x + xOffset;
+
+        transform.localPosition = new Vector3(newXPos, transform.localPosition.y, transform.localPosition.z);
+    }
+}
