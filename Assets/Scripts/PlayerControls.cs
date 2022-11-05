052d73edae483614df2acc177e5d4b66f67cc184
Maxim Kravchenko <max.kravchenko.dev@gmail.com>
2022-11-05 22:05:54 +0200

    Rotate Ship With Position & Throw

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -7,7 +7,14 @@ public class PlayerControls : MonoBehaviour
 {
     [SerializeField] float controlSpeed = 10f;
     [SerializeField] float xRange = 5f;
-    [SerializeField] float yRange = 3.5f;    
+    [SerializeField] float yRange = 3.5f;
+    [SerializeField] float positionPitchFactor = -2f;
+    [SerializeField] float controlPitchFactor = -15f;
+    [SerializeField] float positionYawFactor = 3f;
+    [SerializeField] float controlRollFactor = -20f;       
+
+    float yThrow, xThrow;
+   
     // Update is called once per frame
     void Update()
     {
@@ -16,14 +23,21 @@ public class PlayerControls : MonoBehaviour
     }
 
     void ProcessRotation()
-    {
-        transform.localRotation = Quaternion.Euler(-30f, 30f, 0f); 
+    {   
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControlThrow = yThrow * controlPitchFactor;
+        
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;
+        float yaw = transform.localPosition.x * positionYawFactor;
+        float roll = xThrow * controlRollFactor;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll); 
     }
     
     void ProcessTranslation()
     {
-        float xThrow = Input.GetAxis("Horizontal");
-        float yThrow = Input.GetAxis("Vertical");
+        xThrow = Input.GetAxis("Horizontal");
+        yThrow = Input.GetAxis("Vertical");
 
         float xOffset = xThrow * Time.deltaTime * controlSpeed;
         float rawXPos = transform.localPosition.x + xOffset;
